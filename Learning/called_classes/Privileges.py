ce4cf7be250afffb644d47a5d3e2e84a0e7c1bb4
Carlos Quiroz <carlosquiroz@cArLiToS-mac.local>
2025-12-28 13:21:23 -0500

    Deleted unnecessary files for re-arrengement

--- Learning/called_classes/Privileges.py
+++
@@ -1,9 +0,0 @@
-# A class to print users' privileges
-class Privileges():
-    def __init__(self):
-        self.privileges = ['can add post', 'can delete post', 'can ban user']
-
-    def show_privileges(self):
-        print("Printing Privileges: ")
-        for privilege in self.privileges:
-            print(privilege)
